72cc48846921f7e02554c5eaef9d18bae24732bf
AlexTech314 <alest314@gmail.com>
2024-10-13 15:30:25 -0400

    CDK Tutorial

---
+++ test/selenium_global.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SeleniumGlobal from '../lib/selenium_global-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/selenium_global-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SeleniumGlobal.SeleniumGlobalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
